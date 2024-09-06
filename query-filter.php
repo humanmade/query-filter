ffc0d9bf6d202929c79688063b72cb85094c26ec
Robert O'Rourke <robert@o-rourke.org>
2024-09-06 16:25:05 +0100

    Update plugin header

--- query-filter.php
+++ query-filter.php
@@ -1,8 +1,17 @@
 <?php
 /**
- * Plugin Name: Query Loop Filter
+ * Plugin Name:       Query Loop Filters
+ * Description:       Filter blocks for the query loop utilising the interactivity API.
+ * Requires at least: 6.6
+ * Requires PHP:      8.0
+ * Version:           0.1.0
+ * Author:            Human Made Limited
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       query-filter
+ *
+ * @package           query-filter
  */
-
 namespace HM\Query_Loop_Filter;
 
 const PLUGIN_FILE = __FILE__;
