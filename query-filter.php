626bf3bb5e5a53f5e6d6720450e9b914c6c71c3c
Robert O'Rourke <23417+roborourke@users.noreply.github.com>
2025-02-12 11:19:16 +0000

    Update to 0.2.1 for bug fix release

--- query-filter.php
+++ query-filter.php
@@ -4,7 +4,7 @@
  * Description:       Filter blocks for the query loop utilising the interactivity API.
  * Requires at least: 6.6
  * Requires PHP:      8.0
- * Version:           0.2.0
+ * Version:           0.2.1
  * Author:            Human Made Limited
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
