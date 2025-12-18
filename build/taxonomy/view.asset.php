799687c094758c6fa5a9137fa7e7f432f1e6084a
Alexis J. Villegas <14046668+ajvillegas@users.noreply.github.com>
2025-12-17 21:04:02 -0400

    Commit build files

--- build/taxonomy/view.asset.php
+++ build/taxonomy/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('@wordpress/interactivity', array('id' => '@wordpress/interactivity-router', 'import' => 'dynamic')), 'version' => 'e7ba522c0e73d74a56f1', 'type' => 'module');
+<?php return array('dependencies' => array('@wordpress/interactivity', array('id' => '@wordpress/interactivity-router', 'import' => 'dynamic')), 'version' => '9a6a389e887a62ca9605', 'type' => 'module');
